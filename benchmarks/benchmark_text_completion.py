8736fbfe9ce3e675677f0126e7b8311d70a07fbc
Konstantinos Papaioannou <konstantinos.papaioannou@imdea.org>
2023-12-18 10:36:25 +0100

    Add text competion benchmark
    Signed-off-by: Konstantinos Papaioannou <konstantinos.papaioannou@imdea.org>

---
+++ benchmarks/benchmark_text_completion.py
@@ -0,0 +1,196 @@
+import argparse
+import logging
+import os
+import pickle
+import time
+from typing import List
+
+from tqdm import tqdm
+
+from trace import generate_text_completion_requests
+from utils import get_model_name, get_dataset_name, get_dataset, get_sampling_dir_name
+
+from vllm import LLM, SamplingParams
+
+
+logger = logging.getLogger(__name__)
+
+
+def main(args: argparse.Namespace):
+    llm = LLM(
+        model=args.model,
+        seed=args.seed,
+        gpu_memory_utilization=args.gpu_memory_utilization,
+        swap_space=args.swap_space,
+        block_size=args.block_size,
+        max_num_batched_tokens=args.max_num_batched_tokens,
+        max_num_seqs=args.max_num_sequences,
+        disable_log_stats=not args.log_stats,
+        collect_stats=args.collect_stats,
+        token=args.token,
+    )
+
+    # Generate requests.
+    requests = generate_text_completion_requests(
+        args.dataset,
+        args.request_rate,
+        args.duration,
+        args.seed,
+        args.n1,
+        args.n2,
+        args.n3,
+        args.n4,
+        args.n6,
+        args.n2_beam,
+        args.n4_beam,
+        args.n6_beam,
+        args.n8_beam,
+    )
+
+    # Warm up.
+    logger.info('Warming up.')
+    num_warmup_requests = 8
+    warmup_input_len = 8
+    warmup_output_len = 32
+    warmup_sampling_params = SamplingParams(
+        n=1,
+        best_of=1,
+        temperature=1.0,
+        top_p=0.99,
+        use_beam_search=False,
+        stop_token_ids=set(),
+        max_tokens=warmup_output_len,
+        logprobs=0,
+    )
+
+    llm.generate(
+        sampling_params=warmup_sampling_params,
+        prompt_token_ids=[[0] * warmup_input_len for _ in range(num_warmup_requests)]
+    )
+
+    # Start benchmarking.
+    logger.info('Start benchmarking.')
+    # Initialize tqdm.
+    pbar = tqdm(total=len(requests), desc='Finished requests')
+
+    outputs = []
+    id_time_map = {}
+    llm.llm_engine.scheduler.reset_stats()
+    start_time = time.monotonic()
+    while (requests or llm.llm_engine.has_unfinished_requests()):
+        now = time.monotonic()
+        if args.timeout is not None and now - start_time > args.timeout:
+            logger.info('Timeout. Stop benchmarking.')
+            break
+
+        while requests:
+            if requests[0][0] <= now - start_time:
+                request_time, input_token_ids, sampling_params = requests.pop(0)
+                request_id = str(next(llm.request_counter))
+                llm.llm_engine.add_request(
+                    request_id=request_id,
+                    prompt=None,
+                    sampling_params=sampling_params,
+                    prompt_token_ids=input_token_ids,
+                    arrival_time=start_time + request_time)
+                id_time_map[request_id] = start_time + request_time
+            else:
+                break
+        
+        step_outputs = llm.llm_engine.step()
+
+        now = time.monotonic()
+        for output in step_outputs:
+            if output.finished:
+                arrival_time = id_time_map[output.request_id]
+                finish_time = now
+                outputs.append({
+                    "output": output.to_dict(),
+                    "arrival_time": arrival_time,
+                    "finish_time": finish_time
+                })
+                pbar.update(1)
+
+    pbar.close()
+
+    logger.info('Finish benchmarking. Saving stats.')
+    llm.llm_engine.scheduler.save_stats(args.output_dir)
+    with open(os.path.join(args.output_dir, 'requests.pkl'), 'wb') as f:
+        pickle.dump(outputs, f)
+    
+    logger.info('Done.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Benchmark real text completion workloads.')
+    
+    # LLM parameters
+    parser.add_argument('--model', type=str, default='facebook/opt-125m')
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--gpu-memory-utilization', type=float, required=False)
+    parser.add_argument('--swap-space', type=int, required=False)
+    parser.add_argument('--block-size', type=int, default=16)
+    parser.add_argument('--max-num-batched-tokens', type=int, required=False)
+    parser.add_argument('--max-num-sequences', type=int, required=False)
+    parser.add_argument('--log-stats', action='store_true')
+
+    # Requests parameters
+    parser.add_argument('--dataset', type=str, help='path to dataset', required=True)
+    parser.add_argument('--request-rate', type=float, help='reqs/sec', required=True)
+    parser.add_argument('--duration', type=int, help='duration in seconds', required=True)
+    parser.add_argument('--n1', type=float, help='ratio of requests with n=1', default=0.0)
+    parser.add_argument('--n2', type=float, help='ratio of requests with n=2', default=0.0)
+    parser.add_argument('--n3', type=float, help='ratio of requests with n=3', default=0.0)
+    parser.add_argument('--n4', type=float, help='ratio of requests with n=4', default=0.0)
+    parser.add_argument('--n6', type=float, help='ratio of requests with n=6', default=0.0)
+    parser.add_argument('--n2-beam', type=float, help='ratio of requests with n=2 & beam search', default=0.0)
+    parser.add_argument('--n4-beam', type=float, help='ratio of requests with n=4 & beam search', default=0.0)
+    parser.add_argument('--n6-beam', type=float, help='ratio of requests with n=6 & beam search', default=0.0)
+    parser.add_argument('--n8-beam', type=float, help='ratio of requests with n=8 & beam search', default=0.0)
+
+    # Other parameters
+    parser.add_argument('--timeout', type=int, help='time out in seconds', default=None)
+    parser.add_argument('--output-dir', type=str, help='path to output directory', default=None)
+    parser.add_argument('--collect-stats', action='store_true')
+    parser.add_argument('--token', type=str, help='Hugging Face token', default=None)
+
+    args = parser.parse_args()
+    
+    if args.n1 + args.n2 + args.n3 + args.n4 + args.n6 + args.n2_beam + args.n4_beam + args.n6_beam + args.n8_beam != 1.0:
+        raise ValueError('The ratios of requests must sum to 1.')
+
+    model_name = get_model_name(args.model)
+    dataset_name = get_dataset_name(args.dataset)
+    
+    args.dataset = get_dataset(dataset_name, model_name)
+    
+    sample_dir = get_sampling_dir_name(
+        args.n1, args.n2, args.n3, args.n4, args.n6, args.n2_beam, args.n4_beam, args.n6_beam, args.n8_beam)
+    
+    if args.output_dir is None:
+        args.output_dir = os.path.join(
+            './results',
+            dataset_name,
+            model_name,
+            sample_dir,
+            f'block{args.block_size}',
+            f'req-rate-{args.request_rate}',
+            f'seed{args.seed}',
+            f'duration-{args.duration}',
+        )
+    os.makedirs(args.output_dir, exist_ok=True)
+    
+    # Set up logging.
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(os.path.join(args.output_dir, 'log.txt'), mode="w"),
+        ],
+    )
+    logger.info(args)
+
+    main(args)
---
+++ benchmarks/utils.py
@@ -0,0 +1,95 @@
+import os
+
+OPT_MODELS = {
+    "opt-125m",
+    "opt-350m",
+    "opt-1.3b",
+    "opt-2.7b",
+    "opt-6.7b",
+    "opt-13b",
+}
+
+LLAMA_MODELS = {
+    "Llama-2-7b",
+    "Llama-2-13b",
+}
+
+MISTRAL_MODELS = {
+    "Mistral-7B-v0.1"
+}
+
+FALCON_MODELS = {
+    "falcon-7b"
+}
+
+def get_model_name(model: str) -> str:
+    for model_name in OPT_MODELS.union(LLAMA_MODELS, MISTRAL_MODELS, FALCON_MODELS):
+        if model_name in model:
+            return model_name
+    
+    raise ValueError(f"Unknown model: {model}")
+
+
+def get_dataset(dataset_name, model_name) -> str:
+    dataset_path = os.path.abspath(os.path.join(os.getcwd(), "datasets"))
+    
+    if model_name in OPT_MODELS:
+        dataset_path = os.path.join(dataset_path, "opt")
+    elif model_name in LLAMA_MODELS:
+        dataset_path = os.path.join(dataset_path, "llama")
+    elif model_name in MISTRAL_MODELS:
+        dataset_path = os.path.join(dataset_path, "mistral")
+    elif model_name in FALCON_MODELS:
+        dataset_path = os.path.join(dataset_path, "falcon")
+    else:
+        raise ValueError(f"Unknown dataset {dataset_name} for model {model_name}")
+    
+    dataset_path = os.path.join(dataset_path, f"{dataset_name}_data.pkl")
+    if dataset_name not in ["alpaca", "sharegpt", "cnn_dailymail", "dolly"]:
+        raise ValueError(f"Unknown dataset: {dataset_name}")
+    
+    return dataset_path
+    
+def get_dataset_name(dataset: str) -> str:
+    if "sharegpt" in dataset.lower():
+        return "sharegpt"
+    elif "alpaca" in dataset.lower():
+        return "alpaca"
+    elif "cnn_dailymail" in dataset.lower():
+        return "cnn_dailymail"
+    elif "dolly" in dataset.lower():
+        return "dolly"
+    else:
+        raise ValueError(f"Unknown dataset: {dataset}")
+    
+def get_sampling_dir_name(
+    n1: float,
+    n2: float,
+    n3: float,
+    n4: float,
+    n6: float,
+    n2_beam: float,
+    n4_beam: float,
+    n6_beam: float,
+    n8_beam: float,
+) -> str:
+    method = ""
+    if n1 > 0.0:
+        method = "n1" if n1 == 1.0 else method + f"n1-{n1}-"
+    if n2 > 0.0:
+        method = "n2" if n2 == 1.0 else method + f"n2-{n2}-"
+    if n3 > 0.0:
+        method = "n3" if n3 == 1.0 else method + f"n3-{n3}-"
+    if n4 > 0.0:
+        method = "n4" if n4 == 1.0 else method + f"n4-{n4}-"
+    if n6 > 0.0:
+        method = "n6" if n6 == 1.0 else method + f"n6-{n6}-"
+    if n2_beam > 0.0:
+        method = "n2-beam" if n2_beam == 1.0 else method + f"n2-beam-{n2_beam}-"
+    if n4_beam > 0.0:
+        method = "n4-beam" if n4_beam == 1.0 else method + f"n4-beam-{n4_beam}-"
+    if n6_beam > 0.0:
+        method = "n6-beam" if n6_beam == 1.0 else method + f"n6-beam-{n6_beam}-"
+    if n8_beam > 0.0:
+        method = "n8-beam" if n8_beam == 1.0 else method + f"n8-beam-{n8_beam}-"
+    return method[:-1] if method.endswith("-") else method
